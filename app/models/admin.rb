fae3598d90f5dc7c24b043f2fd31e857bb76d9f6
Martin Lagrange <lagrangemartin@gmail.com>
2015-04-20 23:59:34 +0200

    Validate login existence

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ActiveRecord::Base
 	validates :login, uniqueness: true
 	validates :login, presence: true
+	validates_with EtuUttLoginValidator, on: :create
 
 	def self.is_admin?(login)
 		Admin.exists?(login: login)
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,9 +1,10 @@
 class Participant < ActiveRecord::Base
 	validates :login, uniqueness: true
 	validates :login, presence: true
+	validates_with EtuUttLoginValidator, on: :create
 
 	def to_param
 		login
 	end
-
+	
 end
