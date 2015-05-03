6c41655a0a409fcca3d521a3e677b040780a40c5
Martin Lagrange <lagrangemartin@gmail.com>
2015-05-03 10:01:40 +0200

    Add Game model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420221311) do
+ActiveRecord::Schema.define(version: 20150503080117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20150420221311) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "beginning"
+    t.datetime "ending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "participants", force: :cascade do |t|
     t.string   "login"
---
+++ spec/factories/games.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :game do
+    beginning "2015-05-03 10:01:17"
+ending "2015-05-03 10:01:17"
+  end
+
+end
