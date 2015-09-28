a18f1bc8750a4a41e79be2e24043ff2c9ae3604b
Martin Lagrange <lagrangemartin@gmail.com>
2015-09-28 23:38:31 +0100

    Add super admin field

---
+++ db/migrate/20150928220134_add_super_admin_field_to_admin.rb
@@ -0,0 +1,5 @@
+class AddSuperAdminFieldToAdmin < ActiveRecord::Migration
+  def change
+    add_column :admins, :super_admin, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503175101) do
+ActiveRecord::Schema.define(version: 20150928220134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "login"
+    t.boolean  "super_admin"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
